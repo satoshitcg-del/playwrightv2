9e5fff17915a6e1f2b83ab8f3ca76db6ded76f31
satoshitcg-del <satoshi.tcg@orangeworking.com>
2026-03-09 02:24:13 +0700

    Update products.sit.js

--- config/products.sit.js
+++ config/products.sit.js
@@ -4,17 +4,17 @@ module.exports = {
   apiUrl: 'https://apixint-sit.askmebill.com',
   
   // Product IDs - SIT (Update these!)
-  products: {
-    'Thai Lotto': 'SIT_PRODUCT_ID',
-    'Super API': 'SIT_PRODUCT_ID',
-    'DIRect_API': 'SIT_PRODUCT_ID',
-    'PGSOFT': 'SIT_PRODUCT_ID',
-    'Fix rate': 'SIT_PRODUCT_ID',
-    'นอกเครือ': 'SIT_PRODUCT_ID',
-    'ในเครือ': 'SIT_PRODUCT_ID',
-    'SportbookV.2': 'SIT_PRODUCT_ID',
-    'Tiamut ในเครือ': 'SIT_PRODUCT_ID',
-    'Tiamut นอกเครือ': 'SIT_PRODUCT_ID',
+  products:{
+    'Thai Lotto': '696531c79c898fce9f7c1702',
+    'Super API': '696531a572da1252718d1ce9',
+    'DIRect_API': '691f256be479de4d15bf9c4e',
+    'PGSOFT': '68f74572ae095015c6d716f3',
+    'Fix rate': '68f21b4e49423eabd805f7d8',
+    'นอกเครือ': '689d8d31c82e1edd8f4172bf',
+    'ในเครือ': '689308019baa0e160653a93f',
+    'SportbookV.2': '67ea587dcb2767a97df3fb07',
+    'Tiamut ในเครือ': '67ea586acb2767a97df3fb05',
+    'Tiamut นอกเครือ': '67ea5855cb2767a97df3fb03',
   },
   
   // Sub-Products (Update these!)
